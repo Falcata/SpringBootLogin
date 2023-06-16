fae11f359848e38707e93c8d7d75d9c9db6067ac
PiruleroAnton <piruleroanton2017@gmail.com>
2023-06-16 18:13:34 +0200

    nueva disposicion

--- login/src/main/java/com/example/login/LoginController.java
+++ login/src/main/java/com/example/login/LoginController.java
@@ -1,15 +1,22 @@
 package com.example.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 
 public class LoginController {
 	
+	@GetMapping("/index")
 	 public String index() {
 		 return "index";
 		 
 	 }
 
+	@GetMapping("/login")
+	 public String login() {
+		 return "login";
+		 
+	 }
 }
