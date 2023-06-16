1b2c3b784e4f6e8d2f911cc313c00dd14372971e
PiruleroAnton <piruleroanton2017@gmail.com>
2023-06-16 18:06:35 +0200

    añadida pagina web

---
+++ login/src/main/java/com/example/login/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+
+public class LoginController {
+	
+	 public String index() {
+		 return "index";
+		 
+	 }
+
+}
