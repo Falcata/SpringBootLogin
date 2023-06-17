cb5e3fca0c60c607b21305822102b2038fdecae3
PiruleroAnton <piruleroanton2017@gmail.com>
2023-06-17 15:40:28 +0200

    actualizado la pagina de inicio y direccionamiento a login

--- login/src/main/java/com/example/login/LoginController.java
+++ login/src/main/java/com/example/login/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class LoginController {
 	
-	@GetMapping("/index")
+	@GetMapping("/")
 	 public String index() {
 		 return "index";
 		 
